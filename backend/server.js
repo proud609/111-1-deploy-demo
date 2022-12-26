86fc9b1f059311080da132d7e416cd0adacfaa48
chengyu.liou <chengyu.liou@TW168020024M.local>
2022-12-26 21:03:16 +0800

    test

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "development") {
 // define routes
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/', routes);
+app.use('/api/', routes);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
